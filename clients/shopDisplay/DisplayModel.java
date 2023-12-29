dd6a4195e98b81dbba45d25e2590572d8b4d78ca
harrycunningham04 <147438079+harrycunningham04@users.noreply.github.com>
2023-12-29 13:22:50 +0000

    Update DisplayModel.java

--- clients/shopDisplay/DisplayModel.java
+++ clients/shopDisplay/DisplayModel.java
@@ -15,60 +15,64 @@ import java.util.Observable;
 //   if it really has changed
 
 /**
- * Implements the Model of the display client
- * @author  Mike Smith University of Brighton
- * @version 2.0
+ * Only updates the display when there is a change in the order state
+ * @author  Harry Cunningham
+ * @version 3.0
  */
+public class DisplayModel extends Observable {
+    private OrderProcessing theOrder = null;
+    private Map<String, List<Integer>> previousOrderState = null;
 
-public class DisplayModel extends Observable
-{
-  private OrderProcessing theOrder = null;
+    /**
+     * Set up initial connection to the order processing system
+     * @param mf Factory to return an object to access the order processing system
+     */
+    public DisplayModel(MiddleFactory mf) {
+        try {
+            theOrder = mf.makeOrderProcessing();
+        } catch (Exception e) {
+            // Serious error in system (Should not occur)
+            DEBUG.error("ModelOfDisplay: " + e.getMessage());
+        }
+        new Thread(this::backgroundRun).start();
+    }
 
-  /**
-   * Set up initial connection to the order processing system
-   * @param mf Factory to return an object to access the order processing system
-   */
-  public DisplayModel( MiddleFactory mf  )
-  {
-    try                                           // 
-    {      
-      theOrder = mf.makeOrderProcessing();        // Process order
-    } catch ( Exception e )
-    {
-      // Serious error in system (Should not occure)
-      DEBUG.error("ModelOfDisplay: " + e.getMessage() );
+    /**
+     * Run as a thread in the background to continually update the display
+     */
+    public void backgroundRun() {
+        while (true) {
+            try {
+                Thread.sleep(2000);
+                DEBUG.trace("ModelOfDisplay checking for updates");
+                
+                Map<String, List<Integer>> currentOrderState = theOrder.getOrderState();
+                if (!orderStateEquals(previousOrderState, currentOrderState)) {
+                    DEBUG.trace("ModelOfDisplay call view");
+                    setChanged();
+                    notifyObservers(currentOrderState);
+                    previousOrderState = currentOrderState;
+                }
+                
+            } catch (InterruptedException | OrderException e) {
+                DEBUG.error("ModelOfDisplay.run()\n%s\n", e.getMessage());
+            }
+        }
     }
-    new Thread( () -> backgroundRun() ).start();
-    
-  }
-  
- /**
-   * Run as a thread in background to continually update the display
-   */
-  public void backgroundRun()
-  {
-    while ( true )                               // Forever                    
-    {
-     try
-      {
-        Thread.sleep(2000);
-        DEBUG.trace( "ModelOfDisplay call view" );
-        setChanged(); notifyObservers();
-      }
-      catch ( InterruptedException e )
-      {
-        DEBUG.error( "%s\n%s\n",
-                    "ModelOfDisplay.run()",
-                    e.getMessage() );
-      }
+
+    private boolean orderStateEquals(Map<String, List<Integer>> state1, Map<String, List<Integer>> state2) {
+        // Implement your logic to compare order states
+        // For simplicity, assuming both are not null
+        return state1.equals(state2);
     }
-  }
-  
- // Will be called by the viewOfDisplay
- //   when it is told that the view has changed
- public synchronized Map<String, List<Integer> > getOrderState()
-       throws OrderException
- {
-   return theOrder.getOrderState();
- }
+
+    /**
+     * Will be called by the viewOfDisplay
+     * when it is told that the view has changed
+     * @return Map containing the current order state
+     * @throws OrderException if there is an issue with the order processing
+     */
+    public synchronized Map<String, List<Integer>> getOrderState() throws OrderException {
+        return theOrder.getOrderState();
+    }
 }
